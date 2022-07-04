f59ef1501614f227cd481296f346235c23882152
Akshay Laptop <eng.akky@gmail.com>
2022-07-05 00:06:43 +0530

    user creation successful

--- app/src/main/java/com/example/akschat/SignUp.java
+++ app/src/main/java/com/example/akschat/SignUp.java
@@ -1,17 +1,24 @@
 package com.example.akschat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.akschat.databinding.ActivitySignUpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUp extends AppCompatActivity {
     ActivitySignUpBinding binding;
     private FirebaseAuth auth;
-
+    FirebaseDatabase database;
 
 
     @Override
@@ -23,7 +30,27 @@ public class SignUp extends AppCompatActivity {
 
         getSupportActionBar().hide();
         auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
 
+        binding.btnSignUP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                auth.createUserWithEmailAndPassword(
+                        binding.etMailID.getText().toString(), binding.etPassword.getText().toString()).
+                        addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(SignUp.this," OnComplete" +"User Created Successfully", Toast.LENGTH_SHORT).show();
+                        }else {
+                            Toast.makeText(SignUp.this,"OnFailed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
+            }
+        });
 
 
 
